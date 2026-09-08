62069db424713c8bbddde6c2871107f021335ac5
Tyuiu.KhomutovaLE@outlook.com <dereminalila@gmail.com>
2026-09-08 17:38:58 +0500

    Task1

---
+++ Tyuiu.KhomutovaLE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.KhomutovaLE.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Лидия";
+            int age = 18;
+
+            Console.WriteLine($"Здравствуйте, меня зовут {name}, мне {age} лет");
+
+            Console.ReadLine();
+        }
+    }
+}
