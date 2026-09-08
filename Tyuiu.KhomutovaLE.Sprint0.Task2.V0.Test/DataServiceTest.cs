0cc2160e8b3448136ad62872352179820e0968ca
Tyuiu.KhomutovaLE@outlook.com <dereminalila@gmail.com>
2026-09-08 18:49:39 +0500

    Task2

---
+++ Tyuiu.KhomutovaLE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KhomutovaLE.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KhomutovaLE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Лидия";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Лидия", res);
+        }
+    }
+}
